4646e06cb8bf1f9bec03612212d82e75c2f3e9a2
mbStavola <mstavola9@gmail.com>
2015-12-02 10:12:50 -0500

    Main activity cleanup

--- app/src/main/java/com/juangm/retrofit_weather_demo/MainActivity.java
+++ app/src/main/java/com/juangm/retrofit_weather_demo/MainActivity.java
@@ -22,15 +22,25 @@ import retrofit.Response;
 import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpToolbar();
+        setUpFab();
+
+        textView = (TextView) findViewById(R.id.resulttext);
+    }
+
+    private void setUpToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final TextView textView = (TextView) findViewById(R.id.resulttext);
+    }
 
+    private void setUpFab() {
         //FAB onClick
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -63,27 +73,5 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
